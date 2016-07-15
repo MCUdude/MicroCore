b00180d925eabc757a97eb3be9b7e5858dd1d6c1
Hans <hansibull@gmail.com>
2016-07-16 01:19:58 +0200

    Cleanup and added SAFEMODE

--- attiny13/avr/cores/core13/wiring_digital.c
+++ attiny13/avr/cores/core13/wiring_digital.c
@@ -4,7 +4,7 @@ Arduino core designed for Attiny13 and similar devices.
 NO WARRANTEE OR GUARANTEES!
 Written by John "smeezekitty" 
 You are free to use, redistribute and modify at will EXCEPT IF MARKED OTHERWISE IN A PARTICULAR SOURCE FILE!
-Version 0.22
+Version v0.3.0
 // ATMEL ATTINY13 - ARDUINO
 //
 //                  +-\/-+
@@ -16,33 +16,52 @@ Version 0.22
 */
 #include "wiring_private.h"
 
-void pinMode(uint8_t pin, uint8_t mode){
-	if(pin > 5 || pin < 0){return;}
-	if(!mode){
+
+void pinMode(uint8_t pin, uint8_t mode)
+{
+	#ifdef SAFEMODE
+		if(pin > 5 || pin < 0)
+			return;
+	#endif
+	if(!mode) // Pin as input
 		DDRB &= ~_BV(pin);
-	} else {
+	else // Pin as output
         DDRB |= _BV(pin);
 	}
 }
-void turnOffPWM(uint8_t timer){
-	if(timer == 0){
+
+
+void turnOffPWM(uint8_t timer)
+{
+	if(timer == 0)
 		TCCR0A &= ~_BV(COM0A1);
-	}
-	if(timer == 1){
+	if(timer == 1)
 		TCCR0A &= ~_BV(COM0B1);
-	}
 }
-void digitalWrite(uint8_t pin, uint8_t val){
-	if(pin > 5 || pin < 0){return;}
-	if(pin<2){turnOffPWM(pin);} //If its a PWM pin, make sure the PWM is off
-	if(!val){
+
+
+void digitalWrite(uint8_t pin, uint8_t val)
+{
+	#ifdef SAFEMODE
+		if(pin > 5 || pin < 0)
+			return;
+		if(pin < 2)
+			turnOffPWM(pin); //If its a PWM pin, make sure the PWM is off
+	#endif		
+	if(!val)
 		PORTB &= ~_BV(pin);
-	} else {
+	else
 		PORTB |= _BV(pin);
-	}
 }
-uint8_t digitalRead(uint8_t pin){
-	if(pin > 5 || pin < 0){return 0;}
-	if(pin < 2) turnOffPWM(pin); //If its PWM pin, makes sure the PWM is off
+
+
+uint8_t digitalRead(uint8_t pin)
+{
+	#ifdef SAFEMODE
+		if(pin > 5 || pin < 0)
+			return 0;
+		if(pin < 2)
+			turnOffPWM(pin); //If its PWM pin, makes sure the PWM is off
+	#endif		
 	return !!(PINB & _BV(pin));
 }
