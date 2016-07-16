a5dcdba1e2f2e431d70fb5723ca874d671d03a0e
MCUdude <mcudude@users.noreply.github.com>
2016-07-16 19:20:57 +0200

    These are not needed anymore

--- avr/cores/core13/Arduino.h
+++ avr/cores/core13/Arduino.h
@@ -27,15 +27,12 @@ Modified by MCUdude
 #ifdef __cplusplus
 extern "C"{
 #endif
-  #define SAFEMODE
   #define HIGH 1
   #define LOW 0
   #define INPUT 0
   #define OUTPUT 1
-  #define false 0
   #define FALSE 0
   #define TRUE 1
-  #define true 1
   #define PI 3.1416
   #define HALF_PI 1.57
   #define TWO_PI 6.283
