34c4c8a87f26d3fed8ba552fc3b263c4cf0cc601
Hans <hansibull@gmail.com>
2016-07-16 11:53:32 +0200

    Update main.cpp

--- attiny13/avr/cores/core13/main.cpp
+++ attiny13/avr/cores/core13/main.cpp
@@ -1,22 +1,24 @@
-/*
-*** Core13 ***
-Arduino core designed for Attiny13 and similar devices.
+/*** Core13 - main.cpp ***
+Arduino core designed for ATtiny13
 NO WARRANTEE OR GUARANTEES!
-Written by John "smeezekitty" 
 You are free to use, redistribute and modify at will EXCEPT IF MARKED OTHERWISE IN A PARTICULAR SOURCE FILE!
-Version 0.22
-// ATMEL ATTINY13 - ARDUINO
-//
-//                  +-\/-+
-// AinX (D X) PB5  1|    |8  Vcc
-// AinX (D X) PB3  2|    |7  PB2 (D X)  AinX
-// AinX (D X) PB4  3|    |6  PB1 (D X) PWM
-//            GND  4|    |5  PB0 (D X) PWM
-//                  +----+
-*/
-#include <WProgram.h>
-int main(){
-	init(); //Call init routine 
-	setup(); //Call user provided setup()
-	while(1){loop();} //Continuiously call user provided loop()
+
+Written by John "smeezekitty" 
+Modified by MCUdude
+
+ 	  ATTINY13 - ARDUINO
+               +-\/-+
+ A0 (D4) PB5  1|    |8  Vcc
+ A3 (D3) PB3  2|    |7  PB2 (D2)  A1
+ A2 (D5) PB4  3|    |6  PB1 (D1) PWM
+         GND  4|    |5  PB0 (D0) PWM
+               +----+
+*/  
+#include <Arduino.h>
+int main(void)
+{
+	init();  // Call init routine 
+	setup(); // Call user provided setup()
+	while(1) // Continuiously call user provided loop()
+		loop(); 
 }
