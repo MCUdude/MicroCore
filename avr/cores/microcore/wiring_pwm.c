3404106818eb1e7203a5f08bb90aa7fe1646605f
MCUdude <mcudude@users.noreply.github.com>
2016-09-22 19:21:28 +0200

    Create wiring_pwm.c

---
+++ avr/cores/microcore/wiring_pwm.c
@@ -0,0 +1,40 @@
+/*** MicroCore - wiring_PWM.c ***
+An Arduino core designed for ATtiny13
+File created and maintained by MCUdude
+https://github.com/MCUdude/MicroCore
+
+This file contains the PWM function
+analogWrite().
+*/
+
+#include "wiring_private.h"
+
+void analogWrite(uint8_t pin, uint8_t val)
+{
+	#ifdef SAFEMODE
+		DDRB |= _BV(pin & 0x02); // Set the correct pin as output
+	#endif
+		
+	if(val == 0) // Handle off condition
+		digitalWrite(pin, LOW);
+	else if(val == 255) // Handle on condition
+		digitalWrite(pin, HIGH);
+	else // Otherwise setup the appropriate timer compare 
+	{ 
+		if(pin == 1)		
+		{
+			TCCR0A |= _BV(COM0B1);
+			OCR0B = val;
+		}
+		
+		#ifdef SAFEMODE
+		else if(pin == 0)
+		#else
+		else // We're saving a few bytes by not asking if pin = 0
+		#endif
+		{
+			TCCR0A |= _BV(COM0A1);
+			OCR0A = val;
+		}
+	}
+}
