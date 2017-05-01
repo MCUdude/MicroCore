46285a3e43539ac9e7b14d2a676ef08e1ab22123
MCUdude <mcudude@users.noreply.github.com>
2017-05-01 14:48:45 +0200

    Micros warning was a failure.. The compiler allways ran into the error, even if the code didn't included the pulseIn function

--- avr/cores/microcore/wiring_pulse.c
+++ avr/cores/microcore/wiring_pulse.c
@@ -4,17 +4,14 @@ Based on the work done by "smeezekitty"
 Modified and maintained by MCUdude
 https://github.com/MCUdude/MicroCore
 
-This file contains the pulseIn() function.
+This file contains the pulseIn() function,
+which depend on micros().
 */
 
 #include "wiring_private.h"
 
 uint32_t pulseIn(uint8_t pin, uint8_t stat, uint32_t timeout)
-{
-  #if !defined(ENABLE_MICROS)
-    #error Cannot run pulseIn without micros(), which is disabled by default. Enable micros() in the core_settings.h file.
-  #endif
-    
+{ 
   uint32_t st,to;
   to = micros();
   while(digitalRead(pin) == stat){if((micros() - to) > timeout){return 0;}}
