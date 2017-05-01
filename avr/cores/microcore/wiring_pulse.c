7e51d1ea2eef5b9190c5164abe07fe7fcc49a681
MCUdude <mcudude@users.noreply.github.com>
2017-05-01 13:09:17 +0200

    Add proper error messsage if micros() are missing

--- avr/cores/microcore/wiring_pulse.c
+++ avr/cores/microcore/wiring_pulse.c
@@ -11,6 +11,10 @@ This file contains the pulseIn() function.
 
 uint32_t pulseIn(uint8_t pin, uint8_t stat, uint32_t timeout)
 {
+  #if !defined(ENABLE_MICROS)
+    #error Cannot run pulseIn without micros(), which is disabled by default. Enable micros() in the core_settings.h file.
+  #endif
+    
   uint32_t st,to;
   to = micros();
   while(digitalRead(pin) == stat){if((micros() - to) > timeout){return 0;}}
