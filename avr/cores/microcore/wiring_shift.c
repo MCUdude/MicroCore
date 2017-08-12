fb9051e43246dffe286704f402b10d08be0ffb26
MCUdude <mcudude@users.noreply.github.com>
2017-08-12 21:01:21 +0200

    Improve shiftIn and shiftOut speed shiftOut is now 4x faster and shiftIn is 7x faster

--- avr/cores/microcore/wiring_shift.c
+++ avr/cores/microcore/wiring_shift.c
@@ -4,35 +4,56 @@ Based on the work done by "smeezekitty"
 Modified and maintained by MCUdude
 https://github.com/MCUdude/MicroCore
 
-This file contains shift related functions
-such as shiftIn() and shiftOut().
+This file contains the shift related 
+functions shiftIn() and shiftOut().
 */
 
 #include "wiring_private.h"
+
 uint8_t shiftIn(uint8_t d, uint8_t c, uint8_t bitor) 
-{
+{  
+  #if defined(SAFEMODE)
+    if(c > 5 || d > 5) // Return if pin number is too high
+      return;
+    if(c < 2)
+      turnOffPWM(c); // If it's a PWM pin, make sure PWM is off
+    else if(d < 2)
+      turnOffPWM(d); // If it's a PWM pin, make sure PWM is off
+  #endif  
+ 
   uint8_t value = 0;
-  for(uint8_t i = 0; i < 8; ++i) 
+  
+  for(uint8_t i = 0; i < 8; ++i)
   {
-    digitalWrite(c, HIGH);
-    //if(bitor == LSBFIRST){value |= digitalRead(d) << i;}
-    //else{value |= digitalRead(d) << (7 - i);}
-    value |= (digitalRead(d) << ((bitor == LSBFIRST) ? i : 7 - i));
-    digitalWrite(c, LOW);
+    PORTB |= _BV(c);  // Set pin c high
+    value |= (!!(PINB & _BV(d)) << ((bitor == LSBFIRST) ? i : 7 - i)); // clock in the data to pin d
+    PORTB &= ~_BV(c); // Set pin c low
   }
   return value;
 }
 
+
 void shiftOut(uint8_t d, uint8_t c, uint8_t bitor, uint8_t val)
 {
+  
+  #if defined(SAFEMODE)
+    if(c > 5 || d > 5) // Return if pin number is too high
+      return;
+    if(c < 2)
+      turnOffPWM(c); // If it's a PWM pin, make sure PWM is off
+    else if(d < 2)
+      turnOffPWM(d); // If it's a PWM pin, make sure PWM is off
+  #endif    
+  
   for(uint8_t i = 0; i < 8; i++)  
   {
     if(bitor == LSBFIRST)
-      digitalWrite(d, !!(val & (1 << i)));
+      (!!(val & _BV(i))) ? (PORTB |= _BV(d)) : (PORTB &= ~_BV(d));
     else
-      digitalWrite(d, !!(val & (1 << (7 - i))));
-    digitalWrite(c, HIGH);
-    asm("nop");
-    digitalWrite(c, LOW);    
+      (!!(val & _BV(7 - i))) ? (PORTB |= _BV(d)) : (PORTB &= ~_BV(d));
+
+    PORTB |= _BV(c);  // Set pin c high
+    asm("nop");       // Wait one instruction
+    PORTB &= ~_BV(c); // Set pin c low
   }
 }
