5b15fc34a1bf4b4af6a376b3f0170c3a5d03a466
MCUdude <mcudude@users.noreply.github.com>
2017-09-23 15:40:55 +0200

    Fix issue where micros() calculation where wrong #33

--- avr/cores/microcore/wiring.c
+++ avr/cores/microcore/wiring.c
@@ -49,7 +49,7 @@ uint32_t millis()
 #ifdef ENABLE_MICROS
 volatile uint32_t timer0_overflow = 0;
 
-// This will cause an interrupt every 256/64 clock cycle
+// This will cause an interrupt every 256*64 clock cycle
 ISR(TIM0_OVF_vect)
 {
   timer0_overflow++; // Increment counter by one
@@ -57,63 +57,62 @@ ISR(TIM0_OVF_vect)
 
 uint32_t micros()
 {
-  // Preserve old SREG vale
-  uint8_t oldSREG = SREG;
-  // Disable global interrupts
-  cli();
-  // Variable to hold the return value
   uint32_t x;
+  uint8_t t;  
+  
+  uint8_t oldSREG = SREG; // Preserve old SREG value 
+  t = TCNT0;              // Store timer0 counter value
+  cli();                  // Disable global interrupts
+  x = timer0_overflow;    // Store timer0 overflow count
+  SREG = oldSREG;         // Restore SREG
   
   #if F_CPU == 20000000L
     // Each timer tick is 1/(16MHz/64) = 3.2us long. We multiply the timer0_overflow variable
     // by 256 (bitshift 8 times) and we add the current timer count TCNT0. Since each tick is 3.2us long,
     // we multiply by 3 at the end
-    x = ((timer0_overflow << 8) + TCNT0) * 3;
+    return ((x << 8) + t) * 3;
   #elif F_CPU == 16000000L
     // Each timer tick is 1/(16MHz/64) = 4us long. We multiply the timer0_overflow variable
     // by 256 (bitshift 8 times) and we add the current timer count TCNT0. Since each tick is 4us long,
     // we multiply by 4 at the end
-    x = ((timer0_overflow << 8) + TCNT0) * 4;
+    return ((x << 8) + t) * 4;
   #elif F_CPU == 1200000L
     // Each timer tick is 1/(12MHz/64) = 5.333us long. We multiply the timer0_overflow variable
     // by 256 (bitshift 8 times) and we add the current timer count TCNT0. Since each tick is 5.333us long,
     // we multiply by 5 at the end
-    x = ((timer0_overflow << 8) + TCNT0) * 5;  
+    return ((x << 8) + t) * 5;  
   #elif F_CPU == 9600000L
     // Each timer tick is 1/(9.6MHz/64) = 6.666us long. We multiply the timer0_overflow variable
     // by 256 (bitshift 8 times) and we add the current timer count TCNT0. Since each tick is 6.666us long,
     // we multiply by 7 at the end
-    x = ((timer0_overflow << 8) + TCNT0) * 7;
+    return ((x << 8) + t) * 7;
   #elif F_CPU == 80000000L
     // Each timer tick is 1/(8MHz/64) = 8us long. We multiply the timer0_overflow variable
     // by 256 (bitshift 8 times) and we add the current timer count TCNT0. Since each tick is 8us long,
     // we multiply by 8 at the end
-    x = ((timer0_overflow << 8) + TCNT0) * 8;
+    return ((x << 8) + t) * 8;
   #elif F_CPU == 4800000L
     // Each timer tick is 1/(4.8MHz/64) = 13.333us long. We multiply the timer0_overflow variable
     // by 256 (bitshift 8 times) and we add the current timer count TCNT0. Since each tick is 13.333us long,
     // we multiply by 13 at the end
-    x = ((timer0_overflow << 8) + TCNT0) * 13;
+    return ((x << 8) + t) * 13;
   #elif F_CPU == 1200000L
     // Each timer tick is 1/(1.2MHz/8) = 6.666us long. We multiply the timer0_overflow variable
     // by 256 (bitshift 8 times) and we add the current timer count TCNT0. Since each tick is 6.666us long,
     // we multiply by 7 at the end
-    x = ((timer0_overflow << 8) + TCNT0) * 7;
+    return ((x << 8) + t) * 7;
   #elif F_CPU == 1000000L
     // Each timer tick is 1/(1MHz/8) = 8us long. We multiply the timer0_overflow variable
     // by 256 (bitshift 8 times) and we add the current timer count TCNT0. Since each tick is 8us long,
     // we multiply by 8 at the end
-    x = ((timer0_overflow << 8) + TCNT0) * 8;  
+    return ((x << 8) + t) * 8;  
   #elif F_CPU == 600000L
     // Each timer tick is 1/(600kHz/8) = 13.333us long. We multiply the timer0_overflow variable
     // by 256 (bitshift 8 times) and we add the current timer count TCNT0. Since each tick is 13.333us long,
     // we multiply by 13 at the end
-    x = ((timer0_overflow << 8) + TCNT0) * 13;    
+    return ((x << 8) + t) * 13;    
  #endif
- 
-  // Restore SREG
-  SREG = oldSREG;
-  return x;
+
 }
 #endif // ENABLE_MICROS
 
