45c0d923c1601d85cae87becbac59ab98afd3793
MCUdude <mcudude@users.noreply.github.com>
2017-10-21 22:47:51 +0200

    Change parameter names to make code more understandable

--- avr/cores/microcore/wiring_shift.c
+++ avr/cores/microcore/wiring_shift.c
@@ -10,50 +10,50 @@ functions shiftIn() and shiftOut().
 
 #include "wiring_private.h"
 
-uint8_t shiftIn(uint8_t d, uint8_t c, uint8_t bitor) 
+uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder)
 {  
   #if defined(SAFEMODE)
-    if(c > 5 || d > 5) // Return if pin number is too high
+    if(clockPin > 5 || datapin > 5) // Return if pin number is too high
       return;
-    if(c < 2)
-      turnOffPWM(c); // If it's a PWM pin, make sure PWM is off
-    else if(d < 2)
-      turnOffPWM(d); // If it's a PWM pin, make sure PWM is off
+    if(clockPin < 2)
+      turnOffPWM(clockPin); // If it's a PWM pin, make sure PWM is off
+    else if(dataPin < 2)
+      turnOffPWM(dataPin);  // If it's a PWM pin, make sure PWM is off
   #endif  
  
   uint8_t value = 0;
   
   for(uint8_t i = 0; i < 8; ++i)
   {
-    PORTB |= _BV(c);  // Set pin c high
-    value |= (!!(PINB & _BV(d)) << ((bitor == LSBFIRST) ? i : 7 - i)); // clock in the data to pin d
-    PORTB &= ~_BV(c); // Set pin c low
+    PORTB |= _BV(clockPin);  // Set clockPin high
+    value |= (!!(PINB & _BV(dataPin)) << ((bitOrder == LSBFIRST) ? i : 7 - i)); // clock in to dataPin
+    PORTB &= ~_BV(clockPin); // Set clockPin low
   }
   return value;
 }
 
 
-void shiftOut(uint8_t d, uint8_t c, uint8_t bitor, uint8_t val)
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val)
 {
   
   #if defined(SAFEMODE)
-    if(c > 5 || d > 5) // Return if pin number is too high
+    if(clockPin > 5 || dataPin > 5) // Return if pin number is too high
       return;
-    if(c < 2)
-      turnOffPWM(c); // If it's a PWM pin, make sure PWM is off
-    else if(d < 2)
-      turnOffPWM(d); // If it's a PWM pin, make sure PWM is off
+    if(clockPin < 2)
+      turnOffPWM(clockPin); // If it's a PWM pin, make sure PWM is off
+    else if(dataPin < 2)
+      turnOffPWM(dataPin);  // If it's a PWM pin, make sure PWM is off
   #endif    
   
   for(uint8_t i = 0; i < 8; i++)  
   {
-    if(bitor == LSBFIRST)
-      (!!(val & _BV(i))) ? (PORTB |= _BV(d)) : (PORTB &= ~_BV(d));
+    if(bitOrder == LSBFIRST)
+      (!!(val & _BV(i))) ? (PORTB |= _BV(dataPin)) : (PORTB &= ~_BV(dataPin));
     else
-      (!!(val & _BV(7 - i))) ? (PORTB |= _BV(d)) : (PORTB &= ~_BV(d));
+      (!!(val & _BV(7 - i))) ? (PORTB |= _BV(dataPin)) : (PORTB &= ~_BV(dataPin));
 
-    PORTB |= _BV(c);  // Set pin c high
-    asm("nop");       // Wait one instruction
-    PORTB &= ~_BV(c); // Set pin c low
+    PORTB |= _BV(clockPin);  // Set pin clockPin high
+    asm("nop");              // Wait one instruction
+    PORTB &= ~_BV(clockPin); // Set clockPin low
   }
 }
