0c947a8a9580afa6880addcb0178e89abe2d4c62
MCUdude <mcudude@users.noreply.github.com>
2017-10-21 23:25:28 +0200

    Update Arduino.h

--- avr/cores/microcore/Arduino.h
+++ avr/cores/microcore/Arduino.h
@@ -53,21 +53,21 @@ extern "C"{
   #endif
   #define min(a,b) ((a)<(b)?(a):(b))
   #define max(a,b) ((a)>(b)?(a):(b))
-  #define abs(x) ((x)>0?(x):-(x))
+  #define abs(x)   ((x)>0?(x):-(x))
   #define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
   #define round(x)     ((x)>=0?(int32_t)((x)+0.5):(int32_t)((x)-0.5))
   #define radians(deg) ((deg)*DEG_TO_RAD)
   #define degrees(rad) ((rad)*RAD_TO_DEG)
-  #define sq(x) ((x)*(x))
+  #define sq(x)        ((x)*(x))
   #define interrupts() sei()
   #define noInterrupts() cli()
-  #define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
+  #define clockCyclesPerMicrosecond()  ( F_CPU / 1000000L )
   #define clockCyclesToMicroseconds(a) ( ((a) * 1000L) / (F_CPU / 1000L) )
   #define microsecondsToClockCycles(a) ( ((a) * (F_CPU / 1000L)) / 1000L )
-  #define lowByte(w) ((uint8_t) ((w) & 0xff))
+  #define lowByte(w)  ((uint8_t) ((w) & 0xff))
   #define highByte(w) ((uint8_t) ((w) >> 8))
-  #define bitRead(value, bit) (((value) >> (bit)) & 0x01)
-  #define bitSet(value, bit) ((value) |= (1UL << (bit)))
+  #define bitRead(value, bit)  (((value) >> (bit)) & 0x01)
+  #define bitSet(value, bit)   ((value) |= (1UL << (bit)))
   #define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
   #define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
   #define delayMicroseconds(us) _delay_us(us) // _delay_us function wrapper
@@ -76,40 +76,50 @@ extern "C"{
   typedef uint8_t byte;
   typedef bool boolean;
   void init(void);
-  void pinMode(uint8_t, uint8_t);
-  void digitalWrite(uint8_t, uint8_t);
-  void delay(uint16_t);
-  uint8_t digitalRead(uint8_t);
-  int16_t analogRead(uint8_t); 
+  void pinMode(uint8_t pin, uint8_t mode);
+  void digitalWrite(uint8_t pin, uint8_t val);
+  void delay(uint16_t ms);
+  uint8_t digitalRead(uint8_t pin);
+  int16_t analogRead(uint8_t pin); 
   void analogReference(uint8_t mode);
   void turnOffPWM(uint8_t pin);
-  void analogWrite(uint8_t, uint8_t);
+  void analogWrite(uint8_t pin, uint8_t val);
   uint32_t millis(void);
   uint32_t micros(void);
-  void shiftOut(uint8_t, uint8_t, uint8_t, uint8_t);
-  uint8_t shiftIn(uint8_t, uint8_t, uint8_t);
-  uint32_t pulseIn(uint8_t pin, uint8_t stat, uint32_t timeout);
-  void attachInterrupt(uint8_t, void (*)(void), uint8_t mode);
-  void detachInterrupt(uint8_t);
+  void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val);
+  uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder);
+  uint32_t pulseIn(uint8_t pin, uint8_t state, uint32_t timeout);
+  void attachInterrupt(uint8_t interruptNum, void (*)(void), uint8_t mode);
+  void detachInterrupt(uint8_t interruptNum);
   void setup(void);
   void loop(void);
+  
 #ifdef __cplusplus
 } // extern "C"
 
 
 
 #ifdef __cplusplus
+  // Tone functions
+  void tone(uint8_t pin, uint16_t frequency, uint32_t duration = 0);
+  void toneRaw(uint8_t pin, uint8_t midPoint, uint32_t lengthTicks, uint8_t prescaleBitMask);
+  void noTone(uint8_t pin = 255);
+  void stopTone(void);
+ 
   uint16_t makeWord(uint16_t w);
   uint16_t makeWord(uint8_t h, uint8_t l);
   #define word(...) makeWord(__VA_ARGS__)
-  int32_t random(int32_t);
-  int32_t random(int32_t, int32_t);
-  void randomSeed(uint16_t);
+  
+  // Math functions
+  int32_t random(int32_t howbig);
+  int32_t random(int32_t howsmall, int32_t howbig);
+  void randomSeed(uint16_t seed);
   template <typename A, typename B, typename C, typename D, typename E> 
   int32_t map(A x, B in_min, C in_max, D out_min, E out_max)
   {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   }
+  
 #endif
 
 #endif
