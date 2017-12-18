bcad68dbea76ff2712874232e235a6afccc354a4
MCUdude <mcudude@users.noreply.github.com>
2017-12-18 22:35:04 +0100

    Solves #41 where negative interrupt numbers where allowed

--- avr/cores/microcore/WInterrupts.c
+++ avr/cores/microcore/WInterrupts.c
@@ -26,27 +26,26 @@ void attachInterrupt(uint8_t interruptNum, void (*userFunc)(void), uint8_t mode)
 {
   // SAFEMODE prevents you from inserting an interrupt number that's not supported
   #ifdef SAFEMODE
-  if(interruptNum < EXTERNAL_NUM_INTERRUPTS) {
+  if(interruptNum != EXTERNAL_INTERRUPT_0)
+    return;
   #endif
-    uint8_t SaveSREG = SREG; // Save interrupt flag
+  
+  uint8_t SaveSREG = SREG; // Save interrupt flag
 
-    cli(); // Disable interrupts
+  cli(); // Disable interrupts
     
-    intFunc = userFunc; // access the shared data
+  intFunc = userFunc; // access the shared data
     
-    SREG = SaveSREG; // Restore the interrupt flag
+  SREG = SaveSREG; // Restore the interrupt flag
     
-    // Configure the interrupt mode (trigger on low input, any change, rising
-    // edge, or falling edge).  The mode constants were chosen to correspond
-    // to the configuration bits in the hardware register, so we simply shift
-    // the mode into place.
+  // Configure the interrupt mode (trigger on low input, any change, rising
+  // edge, or falling edge).  The mode constants were chosen to correspond
+  // to the configuration bits in the hardware register, so we simply shift
+  // the mode into place.
       
-    // Enable INT0 on pin PB1
-    MCUCR = (MCUCR & ~((1 << ISC00) | (1 << ISC01))) | (mode << ISC00);
-    GIMSK |= (1 << INT0);
-  #ifdef SAFEMODE  
-  }
-  #endif
+  // Enable INT0 on pin PB1
+  MCUCR = (MCUCR & ~((1 << ISC00) | (1 << ISC01))) | (mode << ISC00);
+  GIMSK |= (1 << INT0);
 }
 
 
@@ -54,16 +53,15 @@ void detachInterrupt(uint8_t interruptNum)
 {
   // SAFEMODE prevents you from inserting an interrupt number that's not supported
   #ifdef SAFEMODE
-  if (interruptNum < EXTERNAL_NUM_INTERRUPTS) 
-  {
+  if(interruptNum != EXTERNAL_INTERRUPT_0)
+    return;
   #endif
-    // Disable INT0 on pin PB1
-    GIMSK &= ~(1 << INT0);
-    intFunc = 0;
-  #ifdef SAFEMODE  
-  }
-  #endif
+  
+  // Disable INT0 on pin PB1
+  GIMSK &= ~(1 << INT0);
+  intFunc = 0;
 }
+
 
 // AttachInterrupt ISR
 ISR(INT0_vect)
