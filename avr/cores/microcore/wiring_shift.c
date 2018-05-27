77edefe781f64350b471bb238b088ef32000c08f
MCUdude <mcudude@users.noreply.github.com>
2018-05-27 21:41:15 +0200

    Improve shiftOut Thanks to @nerdralph we not got an even more optimizedshoftOut function. Closes #62

--- avr/cores/microcore/wiring_shift.c
+++ avr/cores/microcore/wiring_shift.c
@@ -17,7 +17,7 @@ uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder)
       return;
     if(clockPin < 2)
       turnOffPWM(clockPin); // If it's a PWM pin, make sure PWM is off
-    else if(dataPin < 2)
+    if(dataPin < 2)
       turnOffPWM(dataPin);  // If it's a PWM pin, make sure PWM is off
   #endif  
  
@@ -33,27 +33,41 @@ uint8_t shiftIn(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder)
 }
 
 
-void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t val)
+void shiftOut(uint8_t dataPin, uint8_t clockPin, uint8_t bitOrder, uint8_t value)
 {
+  
+  const uint8_t datapinMask = _BV(dataPin);
+  const uint8_t clkpinMask = _BV(clockPin);
+  uint8_t i = 8;
+  uint8_t portbits = (PORTB & ~(datapinMask | clkpinMask));
   
   #if defined(SAFEMODE)
     if(clockPin > 5 || dataPin > 5) // Return if pin number is too high
       return;
     if(clockPin < 2)
       turnOffPWM(clockPin); // If it's a PWM pin, make sure PWM is off
-    else if(dataPin < 2)
+    if(dataPin < 2)
       turnOffPWM(dataPin);  // If it's a PWM pin, make sure PWM is off
   #endif    
-  
-  for(uint8_t i = 0; i < 8; i++)  
+   
+  do
   {
-    if(bitOrder == LSBFIRST)
-      (!!(val & _BV(i))) ? (PORTB |= _BV(dataPin)) : (PORTB &= ~_BV(dataPin));
+    PORTB = portbits; // Clock and data pin low
+    if(bitOrder == MSBFIRST)
+    {
+      if(value & 0x80)
+        PINB = datapinMask;
+    }
     else
-      (!!(val & _BV(7 - i))) ? (PORTB |= _BV(dataPin)) : (PORTB &= ~_BV(dataPin));
-
-    PORTB |= _BV(clockPin);  // Set pin clockPin high
-    asm("nop");              // Wait one instruction
-    PORTB &= ~_BV(clockPin); // Set clockPin low
+    {
+      if(value & 0x01) 
+        PINB = datapinMask;
+    }
+    PINB = clkpinMask; // Toggle clock pin
+    if(bitOrder == MSBFIRST) 
+      value <<= 1;
+    else 
+      value >>= 1;
   }
+  while(--i);
 }
