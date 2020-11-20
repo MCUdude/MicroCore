ef21fcba3b32de7cd77b0021295031b7caf7f16c
MCUdude <hansibull@gmail.com>
2020-11-21 00:11:47 +0100

    Fix typo in folder name

--- avr/cores/microcore/Arduino.h
+++ avr/cores/microcore/Arduino.h
@@ -1,11 +1,11 @@
 /*** MicroCore - Arduino.h ***
 An Arduino core designed for ATtiny13
-Based on the work done by "smeezekitty" 
+Based on the work done by "smeezekitty"
 
 Modified and maintained by MCUdude
 https://github.com/MCUdude/MicroCore
-*/  
-                
+*/
+
 #ifndef Arduino_h
 #define Arduino_h
 
@@ -34,13 +34,6 @@ extern volatile uint32_t timer0_overflow;
 #error Your compiler does not support LTO. Please either upgrade Arduino AVR Boards and/or your IDE
 #endif  //GCC_VERSION < 40902
 
-#define PIN_PB0 0
-#define PIN_PB1 1
-#define PIN_PB2 2
-#define PIN_PB3 3
-#define PIN_PB4 4
-#define PIN_PB5 5
-
 #ifdef __cplusplus
 extern "C"{
 #endif
@@ -57,7 +50,7 @@ extern "C"{
   #define DEG_TO_RAD 0.0174533
   #define RAD_TO_DEG 57.2958
   #define SERIAL 0
-  #define DISPLAY 1 
+  #define DISPLAY 1
   #define LSBFIRST 0
   #define MSBFIRST 1
   #define CHANGE 1
@@ -70,13 +63,20 @@ extern "C"{
   #define TIMER0A 1
   #define TIMER0B 2
 
-  #define min(a,b) ((a)<(b)?(a):(b))
-  #define max(a,b) ((a)>(b)?(a):(b))
-  #define constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
-  #define round(x)     ((x)>=0?(int32_t)((x)+0.5):(int32_t)((x)-0.5))
-  #define radians(deg) ((deg)*DEG_TO_RAD)
-  #define degrees(rad) ((rad)*RAD_TO_DEG)
-  #define sq(x)        ((x)*(x))
+  #ifndef abs
+    #define abs(x)     __builtin_abs(x)
+  #endif
+  #define sq(x)        ({ typeof (x) _x = (x); _x * _x; })
+  #define min(a,b)     ({ typeof (a) _a = (a); typeof (b) _b = (b); _a < _b ? _a : _b;    })
+  #define max(a,b)     ({ typeof (a) _a = (a); typeof (b) _b = (b); _a > _b ? _a : _b;    })
+  #define round(x)     ({ typeof (x) _x = (x);  _x >= 0 ? (long)_x + 0.5 : (long)_x - 0.5 })
+  #define radians(deg) ((deg) * DEG_TO_RAD)
+  #define degrees(rad) ((rad) * RAD_TO_DEG)
+  #define constrain(x,low,high)   ({ \
+    typeof (x) _x = (x);             \
+    typeof (low) _l = (l);           \
+    typeof (high) _h = (h);          \
+    _x < _l ? _l : _x > _h ? _h :_x })
   #define interrupts() sei()
   #define noInterrupts() cli()
   #define clockCyclesPerMicrosecond()  ( F_CPU / 1000000L )
@@ -93,12 +93,14 @@ extern "C"{
   typedef uint16_t word;
   typedef uint8_t byte;
   typedef bool boolean;
+  // main in .init9 saves rjmp instruction
+  __attribute__(( section(".init9") )) int main();
   void init(void);
   void pinMode(uint8_t pin, uint8_t mode);
   void digitalWrite(uint8_t pin, uint8_t val);
   void delay(uint16_t ms);
   uint8_t digitalRead(uint8_t pin);
-  int16_t analogRead(uint8_t pin); 
+  int16_t analogRead(analog_pin_t pin);
   void analogReference(uint8_t mode);
   void turnOffPWM(uint8_t pin);
   void analogWrite(uint8_t pin, uint8_t val);
@@ -113,7 +115,51 @@ extern "C"{
   void loop(void);
   void yield(void) __attribute__ ((weak, alias("__empty")));
   static void __empty() { /* Empty*/ }
- 
+
+  // Constant checks error handler
+  void badArg(const char*) __attribute__((error("")));
+  #define ASSERT_CONST(pin) \
+    if(!__builtin_constant_p(pin)) badArg("Pin must be a constant")
+
+  // Check for valid PWM pin (0 .. 5)
+  __attribute__((always_inline))
+  inline void check_valid_digital_pin(uint8_t pin)
+  {
+    if(__builtin_constant_p(pin))
+    {
+      if(pin >= NUM_DIGITAL_PINS)
+        badArg("Digtial pin out of range");
+    }
+    else
+      badArg("Digital pin must be a constant");
+  }
+
+  // Check for valid analog pin (A0 .. A3)
+  __attribute__((always_inline))
+  inline void check_valid_analog_pin(analog_pin_t pin)
+  {
+    if(__builtin_constant_p(pin))
+    {
+      if(pin >= BAD_ANALOG_PIN)
+        badArg("Analog pin out of range");
+    }
+    else
+      badArg("Analog pin must be a constant");
+  }
+
+  // Check for valid PWM pin (0 .. 1)
+  __attribute__((always_inline))
+  inline void check_valid_pwm_pin(uint8_t pin)
+  {
+    if(__builtin_constant_p(pin))
+    {
+      if(pin > 1)
+        badArg("PWM pin out of range");
+    }
+    else
+      badArg("PWM pin must be a constant");
+  }
+
 #ifdef __cplusplus
 } // extern "C"
 
@@ -127,21 +173,23 @@ extern "C"{
   void toneRaw(uint8_t pin, uint8_t midPoint, uint32_t lengthTicks, uint8_t prescaleBitMask);
   void noTone(uint8_t pin = 255);
   void stopTone(void);
- 
+
   uint16_t makeWord(uint16_t w);
   uint16_t makeWord(uint8_t h, uint8_t l);
   #define word(...) makeWord(__VA_ARGS__)
-  
+
+  uint32_t pulseIn(uint8_t pin, uint8_t state, uint32_t timeout = 1000000L);
+
   // Math functions
   int32_t random(int32_t howbig);
   int32_t random(int32_t howsmall, int32_t howbig);
   void randomSeed(uint16_t seed);
-  template <typename A, typename B, typename C, typename D, typename E> 
-  int32_t map(A x, B in_min, C in_max, D out_min, E out_max)
+  template <typename A, typename B, typename C, typename D, typename E>
+  int32_t map(A in, B in_min, C in_max, D out_min, E out_max)
   {
-    return (int32_t)(x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    return (int32_t)(in - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
   }
-  
+
 #endif
 
 #endif
