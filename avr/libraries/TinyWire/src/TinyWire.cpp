2fc81d57e1ef2768dc25e2e8cd116f1301d74fb6
MCUdude <hansibull@gmail.com>
2021-08-10 10:16:36 +0200

    Update library source files Handle automatic ACK/NACK based on the second parameter in requestFrom() Improve formatting and comments

--- avr/libraries/TinyWire/src/TinyWire.cpp
+++ avr/libraries/TinyWire/src/TinyWire.cpp
@@ -1,7 +1,12 @@
 #include "TinyWire.h"
 
+// Predefined object
 TinyWire Wire;
 
+/**
+ * @brief Construct a new TinyWire object
+ *
+ */
 TinyWire::TinyWire()
 {
 
@@ -12,113 +17,172 @@ void TinyWire::begin()
   // Todo: implement slave mode
 }
 
-void TinyWire::beginTransmission(uint8_t addr)
+/**
+ * @brief Starts an i2c transmission
+ *
+ * @param addr 7-bit slave i2c address
+ * @return uint8_t Transmission status. 0 = success, 2 = NACK
+ */
+uint8_t TinyWire::beginTransmission(uint8_t addr)
 {
   start();
-  status = write(addr << 1);   // Address goes in upper 7 bits
+  twi_status = write(addr << 1); // Address goes in upper 7 bits
+  return twi_status;
 }
 
+/**
+ * @brief Writes a byte to an i2c slave
+ *
+ * @param data Data to write
+ * @return uint8_t Write status. 0 = success, 2 = NACK
+ */
 uint8_t TinyWire::write(uint8_t data)
 {
   rw(data);
   scl_hi();
 
-  uint8_t err;
-  // Optimization trick saves 4 bytes w/ avr-gcc
-  asm volatile ("clr %0" : "=r" (err));
-  // NACK = SDA high
-  if (I2C_PIN & (1 << SDA))
+  uint8_t err = SUCCESS;
+  asm volatile ("clr %0" : "=r" (err)); // Optimization trick saves 4 bytes w/ avr-gcc
+  if (I2C_PIN & (1 << SDA))             // NACK = SDA high
     err = ADDR_NAK;
-  I2C_DDR |= (1 << SCL);          // SCL low
+  I2C_DDR |= (1 << SCL);                // SCL low
 
   return err;
 }
 
-uint8_t TinyWire::requestFrom(uint8_t addr)
+/**
+ * @brief Request to read N bytes from i2c slave
+ *
+ * @param addr 7-bit slave i2c address
+ * @param number_of_bytes number of bytes to read from the i2c slave
+ * @return uint8_t Write status. 0 = success, 2 = NACK
+ */
+uint8_t TinyWire::requestFrom(uint8_t addr, uint16_t number_of_bytes)
 {
   start();
-  return write(addr << 1 | 1);   // Address goes in upper 7 bits
+  read_bytes = number_of_bytes;
+  return write(addr << 1 | 1); // Address goes in upper 7 bits
 }
 
-uint8_t TinyWire::restartRead(uint8_t addr)
+/**
+ * @brief Restarts a request to read N bytes from i2c slave
+ *
+ * @param addr 7-bit slave i2c address
+ * @param number_of_bytes number of bytes to read from the i2c slave
+ * @return uint8_t Write status. 0 = success, 2 = NACK
+ */
+uint8_t TinyWire::restartRead(uint8_t addr, uint16_t number_of_bytes)
 {
   scl_hi();
-  return requestFrom(addr);
+  return requestFrom(addr, number_of_bytes);
 }
 
+/**
+ * @brief End i2c transmission. Has to be executed after a read sequence too.
+ *
+ * @return uint8_t Transmission status. 0 = success, 2 = NACK
+ */
 uint8_t TinyWire::endTransmission()
 {
-  I2C_DDR |= (1 << SDA);          // SDA low
+  I2C_DDR |= (1 << SDA);  // SDA low
   _delay_us(TWICLK(FREQ));
-  I2C_DDR &= ~(1 << SCL);         // Release SCL
-  asm ("lpm" ::: "r0");           // 3 cycle delay
+  I2C_DDR &= ~(1 << SCL); // Release SCL
   _delay_us(TWICLK(FREQ));
-  I2C_DDR &= ~(1 << SDA);         // Release SDA
-  return status;
+  I2C_DDR &= ~(1 << SDA); // Release SDA
+  return twi_status;
 }
 
-// Ack !=0 to send ack
+/**
+ * @brief Read a byte from an i2c slave device where ACK/NACK is handled automatically
+ *
+ * @return uint8_t Data read from the i2c slave
+ */
+uint8_t TinyWire::read()
+{
+  if (read_bytes-- > 1) // Send ACK of we still have data to read
+    return read(1);
+  else
+    return read(0);     // Send NACK if we're reading last byte
+}
+
+/**
+ * @brief Read a byte from an i2c slave device where ACK/NACK is handled manually
+ *
+ * @param ack ACK flag. Pass 1 for ACK, and 0 for NACK
+ * @return uint8_t Data read from the i2c slave
+ */
 uint8_t TinyWire::read(uint8_t ack)
 {
   uint8_t data = rw(0xFF);
 
   if (ack)
-    I2C_DDR |= (1 << SDA);      // SDA low = ACK
+    I2C_DDR |= (1 << SDA); // SDA low = ACK
   scl_hi();
-  asm ("nop");                  // Delay
-  I2C_DDR |= (1 << SCL);        // SCL low
+  I2C_DDR |= (1 << SCL);   // SCL low
 
   return data;
 }
 
-uint8_t TinyWire::readAck()
-{
-  return read(1);
-}
-
-uint8_t TinyWire::readNack()
-{
-  return read(0);
-}
-
+/**
+ * @brief Low level i2c start routine
+ *
+ */
 void TinyWire::start()
 {
-  // Set SDA to output mode for low
-  I2C_DDR |= (1 << SDA);
-  asm ("lpm" ::: "r0");      // 3 cycle delay
-  // Set SCL to output mode for low
-  I2C_DDR |= (1 << SCL);
+  I2C_DDR |= (1 << SDA); // Set SDA to output mode for low
+  asm ("lpm" ::: "r0");  // 3 cycle delay
+  I2C_DDR |= (1 << SCL); // Set SCL to output mode for low
 }
 
+/**
+ * @brief Low level set clock high routine
+ *
+ */
 __attribute__((always_inline)) inline void TinyWire::scl_hi()
 {
   _delay_us(TWICLK(FREQ));
-  // Set SCL to input mode for high
-  I2C_DDR &= ~(1 << SCL);
-  // Check for clock stretching
-  while ((I2C_PIN & (1 << SCL)) == 0);
+  I2C_DDR &= ~(1 << SCL);              // Set SCL to input mode for high
+  while ((I2C_PIN & (1 << SCL)) == 0); // Check for clock stretching
   _delay_us(TWICLK(FREQ));
 }
 
+/**
+ * @brief Low level read/write routine
+ *
+ * @param data Data to send. Pass 0xFF to read
+ * @return uint8_t Data read from the i2c slave
+ */
 uint8_t TinyWire::rw(uint8_t data)
 {
   uint8_t bits = 8;
   do
   {
-    I2C_DDR &= ~(1 << SDA);     // SDA high
+    I2C_DDR &= ~(1 << SDA); // SDA high
     if (!(data & 0x80))
       I2C_DDR |= (1 << SDA);
     scl_hi();
     uint8_t i2c_pin = I2C_PIN;
-    I2C_DDR |= (1 << SCL);      // SCL low
+    I2C_DDR |= (1 << SCL); // SCL low
     data <<= 1;
     if (i2c_pin & (1 << SDA))
       data |= 0x01;
   }
   while (--bits);
 
-  I2C_DDR &= ~(1 << SDA);      // Release SDA
+  I2C_DDR &= ~(1 << SDA); // Release SDA
   return data;
 }
 
-uint8_t TinyWire::status;
+/**
+ * @brief Holds the i2c transmission status shared between
+ * startTransmission() and endTransmission()
+ */
+uint8_t  TinyWire::twi_status;
+
+/**
+ * @brief Holds the number of bytes to read from a slave.
+ * This variable is set in requestFrom() and used to keep track
+ * of ACK/NACK in read().
+ *
+ */
+uint16_t TinyWire::read_bytes;
--- avr/libraries/TinyWire/src/TinyWire.h
+++ avr/libraries/TinyWire/src/TinyWire.h
@@ -1,35 +1,41 @@
 
-#pragma once
+#ifndef TINYWIRE_H
+#define TINYWIRE_H
 
-#include <stdint.h>
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stdint.h>
 
 #ifndef I2C_DDR
   #define I2C_DDR DDRB
   #define I2C_PIN PINB
 #endif
 
+// Crude formula to calculate approximate TWI clock speed based on F_CPU
 #define TWICLK(FREQ) (((1000000.0f / (FREQ / 0.5)) < (12.0 * (1000000.0f / F_CPU)) ? 0.0 : ((1000000.0f / (FREQ / 0.5)) - (12.0 * (1000000.0f / F_CPU)))))
 
-const uint8_t SUCCESS = 0;
-const uint8_t ADDR_NAK = 2;
+// Use TinyWire::FREQ = MAX_SPEED to get the highest speed possible, which is always way less than 2 MHz
+const uint32_t MAX_SPEED = 2000000L;
 
-const uint32_t MAX_SPEED = 1000000L;
+// Return values for beginTransmission and endTransmission
+enum
+{
+  SUCCESS  = 0,
+  ADDR_NAK = 2,
+};
 
 class TinyWire
 {
   public:
     TinyWire();
-    static void begin();
-    static void beginTransmission(uint8_t addr);
-    static uint8_t write(uint8_t data);       // Start must be called first
-    static uint8_t requestFrom(uint8_t addr);
-    static uint8_t restartRead(uint8_t addr);
+    static void    begin();
+    static uint8_t beginTransmission(uint8_t addr);
+    static uint8_t write(uint8_t data);
+    static uint8_t requestFrom(uint8_t addr, uint16_t number_of_bytes = 1);
+    static uint8_t restartRead(uint8_t addr, uint16_t number_of_bytes = 1);
     static uint8_t endTransmission();
-    static uint8_t read(uint8_t ack = 1);   // ack !=0 to send ack
-    static uint8_t readAck();
-    static uint8_t readNack();
+    static uint8_t read();
+    static uint8_t read(uint8_t ack); // ack > 0 to send ACK
 
     static const uint8_t  SCL;  // Port number for SDA pin
     static const uint8_t  SDA;  // Port number for SCL pin
@@ -40,7 +46,10 @@ class TinyWire
     __attribute__((always_inline)) static inline void scl_hi();
     static uint8_t rw(uint8_t data); // Combined i2c read/write routine. Pass 0xFF when reading
 
-    static uint8_t status;
+    static uint8_t  twi_status;
+    static uint16_t read_bytes;
 };
 
 extern TinyWire Wire;
+
+#endif
